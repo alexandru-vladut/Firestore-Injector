1ce4b441a95db625bab8863ad142de2446823ade
Alexandru-Nicolae Vladut <vladut.alexandru12@gmail.com>
2024-12-21 12:50:41 +0200

    modularized script

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 FIREBASE_ADMIN_SDK_PATH = './keys/key-file.json'
-FIREBASE_COLLECTION_NAME = 'users'
+FIRESTORE_COLLECTION_NAME = 'users'
 INPUT_DATA_PATH = './data/data.json'
--- main.py
+++ main.py
@@ -3,16 +3,34 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from config import *
 
-# Replace 'path/to/serviceAccountKey.json' with the actual path to your key file
-cred = credentials.Certificate(FIREBASE_ADMIN_SDK_PATH)
-firebase_admin.initialize_app(cred)
+def read_from_json(file_path):
+    # Load data from JSON file
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    
+    if isinstance(data, dict):  # If it's a single JSON object
+        data = [data]  # Convert it into a JSON array with one object
 
-# Load data from JSON file
-with open(INPUT_DATA_PATH, 'r') as file:
-    data = json.load(file)
+    if not isinstance(data, list):
+        raise ValueError("Invalid JSON format: Expected a JSON array or a single JSON object.")
+    
+    return data
 
-db = firestore.client()
-collection_ref = db.collection(FIREBASE_COLLECTION_NAME)
+def get_firestore_collection(firebase_admin_sdk_path, firestore_collection_name):
+    cred = credentials.Certificate(firebase_admin_sdk_path)
+    firebase_admin.initialize_app(cred)
 
-for event in data:
-    collection_ref.add(event)
+    db = firestore.client()
+    collection_ref = db.collection(firestore_collection_name)
+
+    return collection_ref
+
+def main():
+    data = read_from_json(INPUT_DATA_PATH)
+    firestore_collection = get_firestore_collection(FIREBASE_ADMIN_SDK_PATH, FIRESTORE_COLLECTION_NAME)
+
+    for object in data:
+        firestore_collection.add(object)
+
+if __name__ == "__main__":
+    main()
